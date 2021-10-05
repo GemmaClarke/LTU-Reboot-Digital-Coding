29681ca394639cd8f1a315038fb60e812702a100
GemmaClarke <90418881+GemmaClarke@users.noreply.github.com>
2021-10-05 20:57:22 +0100

    Update main.js
    Add practice comments, variables, console logs and document.write and data logs

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,14 @@
+// This is a single line comment
+
+/*
+This is a multiple
+line comment
+*/
+
+const productName = 'Baked Beans';
+let price = 1.27;
+let quantity = 4;
+let inStock = true;
+let discountAmount = 10;
+
+console.log( 'Product Name:', productName );
