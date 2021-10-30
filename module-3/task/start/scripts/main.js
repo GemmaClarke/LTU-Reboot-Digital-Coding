bfb4a4de6cc4c87f290b267cbe97c76e2fc33ed0
GemmaClarke <90418881+GemmaClarke@users.noreply.github.com>
2021-10-30 14:49:04 +0100

    Update main.js
    Main.js changes

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -1,9 +1,15 @@
-// This is a single line comment
+//This is a single line comment
 
-/*
-This is a multiple
-line comment
+/**This is a multiple 
+* line 
+* comment */
+
+/** 
+ * let age = 40;
+ * const name = 'Gemma';
+ * var address = '123 long avenue';
 */
+
 
 const productName = 'Baked Beans';
 let price = 1.27;
@@ -12,3 +18,38 @@ let inStock = true;
 let discountAmount = 10;
 
 console.log( 'Product Name:', productName );
+console.log( 'Price:', price );
+console.log( 'Quantity:', quantity );
+console.log( 'In Stock:', inStock );
+console.log( 'Discount:', discountAmount );
+
+// typeof shows the different data types
+
+// console.log( typeof productName );
+// console.log( typeof quantity );
+// console.log( typeof inStock );
+// console.log( typeof discountAmount );
+
+//writing on the document and p for a new paragraph
+
+//document.write( "<p>" + productName + "</p>");
+
+// let myName = '';
+// let numOfKids = '1';
+// let isMarried;
+
+// let newName = 'Luke';
+// let newNumOfKids = '1';
+
+//console.log( Boolean(myName) );
+// console.log( Boolean(numOfKids) );
+// console.log( Boolean(isMarried) );
+// console.log( Boolean(null) );
+// console.log( Boolean(undefined) );
+// console.log( Boolean('-------') );
+// console.log( Boolean(newName) );
+// console.log( Boolean(newNumOfKids) );
+
+
+
+
