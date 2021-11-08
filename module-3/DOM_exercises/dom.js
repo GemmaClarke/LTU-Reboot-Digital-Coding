6ffb8b4d82ab8593b775ee8c13c3c583342000b9
GemmaClarke <90418881+GemmaClarke@users.noreply.github.com>
2021-11-08 23:37:41 +0000

    Update dom.js
    DOM exercises

--- module-3/DOM_exercises/dom.js
+++ module-3/DOM_exercises/dom.js
@@ -26,5 +26,46 @@ paragraph.innerHTML = paragraph.innerHTML.split(' ')
 
 const link = document.createElement('a');
     link.setAttribute('href', 'https://forcemipsum.com/');
-    link.textContent = 'Text generated from forcemipsum';
-    document.bodyappendChild( link );
+    link.textContent = 'Text generated from force ipsum';
+    document.body.appendChild( link );
+
+/**
+ * Exercise 03
+ * -----------
+ * Split each new sentence on to a separate line on the paragraph of text. A sentence can be assumed to be a string of text terminated with a period (.).
+ * 
+ * 
+ */
+
+const splitp = paragraph.innerHTML.split('.')
+.join('.</p><p> + </p>');
+
+/**
+ * Exercise 04
+ * -----------
+ *Count the number of words in the paragraph tag and display the count after the heading.
+ You can assume that all words are separated by one singular white space.
+ * 
+ * 
+ */
+
+ const wordCount = paragraph.innerText.split(' ').length;
+ const wordCountElem = document.createElement('div');
+    wordCountElem.innerText = `${wordCount} words`; //271 words
+    document.body.insertBefore( wordCountElem, paragraph);
+
+/**
+ * Exercise 05
+ * -----------
+ Replace all the question marks with a thinking face (🤔 ) and all the ! with an astonished face (😯).
+ * 
+ * 
+ * 
+ */
+ 
+ Array.from(document.querySelectorAll('p') )
+ .forEach( p =>{
+    p.innerHTML = p.innerHTML
+    .replace( /\?/g, '🤔')
+    .replace( /\!/g, '😯')
+ } )
